ac068e5579bb85a009d7b313f04db7943dd9390b
YUQI-1717 <125873508+YUQI-1717@users.noreply.github.com>
2024-06-17 18:39:37 +0800

    Add files via upload

---
+++ week02/index.php
@@ -0,0 +1,15 @@
+<html>
+
+
+<head>
+<meta charset="utf-8">
+<title>Hello</title>
+
+</head>
+
+
+<body bgcolor="#5287A2" background="C.png">
+
+
+</body>
+</html>
---
+++ week02/index2.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>卡納赫拉</title>
+
+</head>
+
+
+<body bgcolor="#5287A2" background="C.png">
+
+<h1>Hello Qi</h1>
+Hello Qi<br/>
+<b>Hello</b> Qi<i>Hello</i> Qi<br/>
+<u>Hello</u> <a href="2.html">Qi Hello</a> Qi<br/>
+
+
+</body>
+</html>
+
+<table border="1" bgcolor="green">
+<tr bgcolor="white"><th>編號</th><th>姓名</th><th>學號</th></tr>
+<tr><td>1</td><td>麥麥</td><td>Cat1</td></tr>
+
+
+
+</table>
